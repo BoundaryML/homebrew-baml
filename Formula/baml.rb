deaeae582fbfe00e3caa860ccc8b97f287e7f512
Aaron Villalpando <aaron.vlpn@gmail.com>
2023-11-09 12:08:28 -0800

    update baml

--- Formula/baml.rb
+++ Formula/baml.rb
@@ -6,10 +6,10 @@ class Baml < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/GlooHQ/baml/releases/download/unstable%2Fcli%2Fv0.1.1/baml-apple-intel.tar.gz"
-      sha256 "095c2af731624bc22a0c843de27ffc891d4930c62b7df35e0f95deec483c0634"
+      sha256 "7457b946c8716400109ae76310dc9d802e7204ada29f5e90c646be7a037274d3"
     elsif Hardware::CPU.arm?
       url "https://github.com/GlooHQ/baml/releases/download/unstable%2Fcli%2Fv0.1.1/baml-apple-arm.tar.gz"
-      sha256 "b77619efc5b1fe410dd2f685c9789454af938b1c50d8de4cbed13a8f692c550b"
+      sha256 "e4e0cebbc678562461a875f07f382c68a85128666636b4847c2a39b55ae84896"
     end
   end
 
